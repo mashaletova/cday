fbd060b1539c0c24a3efc35a8272a361069a87d5
Masha Nemtinova <mashanemtinova@hotmail.com>
2017-04-20 14:52:41 +0300

    Fixed SQL queries in report.py

--- report.py
+++ report.py
@@ -7,8 +7,7 @@ c = db.c.cursor()
 def humidity():
     c.execute("""select avg(dp.ch_value)
                                  from datastreams ds join datapoints dp on dp.ds_id = ds.ds_id
-                                 where dp.ch_timestamp like '%2017-04-19%'
-                                 and ds.ds_deployment = 'indoor'
+                                 where ds.ds_deployment = 'indoor'
                                  and ds.ds_kind != 'h';""")
     return c.fetchall()
 
@@ -17,7 +16,7 @@ def minmax():
                                  from datapoints dp join datastreams ds on dp.ds_id = ds.ds_id
                                  where ds.ds_deployment = 'outdoor'
                                  and ds.ds_location in (1,2,3,4,5)
-                                 and date(dp.ch_timestamp) between date("2017-04-16") AND date("2017-04-19");""")
+                                 and date(dp.ch_timestamp) between date("2017-04-16") AND date("2017-04-30");""")
     return c.fetchall()
 
 
